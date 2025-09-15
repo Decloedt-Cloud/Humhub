0ba59d4d8e36a1222a02ad238f4e0609599ddccd
Khalil Abbaoui <khalil.abbaoui@decloedt.cloud>
2025-09-15 17:12:58 +0100

    changement dans humhub-app.css

--- humhub-1.17.2/protected/config/web.php
+++ humhub-1.17.2/protected/config/web.php
@@ -1,16 +1,23 @@
 <?php
 /**
- * This file provides to overwrite the default HumHub / Yii configuration by your local Web environments
- * @see http://www.yiiframework.com/doc-2.0/guide-concept-configurations.html
- * @see https://docs.humhub.org/docs/admin/advanced-configuration
+ * Overwrite HumHub / Yii configuration by your local Web environments
  */
+use yii\base\Application;
+
 return [
     'components' => [
-    'response' => [
-        'on beforeSend' => function ($event) {
-            unset($event->sender->headers['X-Frame-Options']);
-        },
+        'response' => [
+            'on beforeSend' => function ($event) {
+                unset($event->sender->headers['X-Frame-Options']);
+            },
+        ],
     ],
-],
+    // Ajouter un event global pour changer la langue
+    'on beforeRequest' => function ($event) {
+        $lang = $_GET['lang'] ?? null;
+        if ($lang) {
+            Yii::$app->language = $lang;
+            Yii::$app->session->set('language', $lang);
+        }
+    },
 ];
-
